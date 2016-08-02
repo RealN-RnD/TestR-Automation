4c1e056c6e634ed40de053d275379b03a177724b
Phildor92 <phillip.evans09@hotmail.com>
2016-08-02 16:18:26 +0200

    Added Mac code v1.1

---
+++ MacTestRail1.1.sikuli/MacTestRail1.1.py
@@ -0,0 +1,23 @@
+while True:
+    click("1470125507873.png")
+    popup("Press OK when you have finished testing")
+    click("1470125519799.png")
+    click("1470046377455.png")
+    paste("Mac OS X 10.10.5" + Key.ENTER + "MacBook Pro (Retina 13-inch Late 2013)" + Key.ENTER + "Model A1502")
+    click("1470046406673.png")
+    click("1470046165953.png")
+    paste("1.14.0.26")
+    click("1470046187921.png")
+    type("Mac OS X 10.10")
+    answer = popAsk("Did the test pass?")
+    if not answer:
+        exit(1)
+    wait(4)
+    click("1470126482063.png")
+    wait(2)
+    click("1470126608431.png")
+    doWeContinue = popAsk("Another test?")
+    if not doWeContinue:
+        exit(1)
+
+
